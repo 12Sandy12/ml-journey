a81e1735f4d9623ef65f90b5328d6dc7195b2342
sandesh <sandeshkhatri460@gmail.com>
2026-05-22 15:47:14 -0400

    Day 1: Python basics - variables, types, f-strings

---
+++ week1/exercises/day1_basics.py
@@ -0,0 +1,22 @@
+a= 0;
+print(type(a));
+
+a= 'a';
+print(type(a));
+
+
+s = a + "1000";
+print(type(s))
+
+print(f"hello world {a}")
+
+
+tempt = 26.4
+
+print(f"The temperature is {tempt} degree celcius in New York today.")
+
+#asking user input
+
+tempt = input(f"Enter temperature: ")
+
+print(f"the temperature for today is {tempt}")
